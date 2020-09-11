1c621d054283616363800c7a11b445698b403a5a
Jin IGARASHI <wushilan0623@gmail.com>
2020-09-11 15:05:38 +0900

    updated legend plugin

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ $(function(){
                 map_.removeControl(legendCtrl);
                 legendCtrl = null;
             }
-            legendCtrl = new MapboxLegendControl(config.legend.targets);
+            legendCtrl = new MapboxLegendControl(config.legend.targets, {showDefault: false});
             map_.addControl(legendCtrl, 'bottom-left')
         });
     }
