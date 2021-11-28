569b34c9d43408a56f7495eed78950403aaab16d
Jin Igarashi <igarashi.jin@water-gis.com>
2021-11-28 14:29:26 +0000

    updated plugins

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import MapboxPitchToggleControl from '@watergis/mapbox-gl-pitch-toggle-control';
 import '@watergis/mapbox-gl-pitch-toggle-control/css/styles.css';
 import MapboxAreaSwitcherControl from '@watergis/mapbox-gl-area-switcher';
 import '@watergis/mapbox-gl-area-switcher/css/styles.css';
-import MapboxLegendControl from "@watergis/mapbox-gl-legend";
+import { MapboxLegendControl } from "@watergis/mapbox-gl-legend";
 import '@watergis/mapbox-gl-legend/css/styles.css';
 import { MapboxExportControl}  from "@watergis/mapbox-gl-export";
 import '@watergis/mapbox-gl-export/css/styles.css';
